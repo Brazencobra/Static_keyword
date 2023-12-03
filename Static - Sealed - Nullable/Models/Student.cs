74670ead64c02acdf25eb77e580e79cf8edc91c7
User <elxan.memmedli2003@gmail.com>
2023-12-03 23:10:04 +0400

    First commit

---
+++ Static - Sealed - Nullable/Models/Student.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static___Sealed___Nullable.Models
+{
+    internal class Student
+    {
+        private static int _count;
+        private readonly int _id;
+        private string _fullname;
+
+        public string Fullname 
+        {
+            get => _fullname;
+            set
+            {
+                _fullname = value;
+            } 
+        }
+        public int Id 
+        {
+            get;
+        }
+        public int Count 
+        {
+            get;
+        }
+
+        public Student(string fullname)
+        {
+            Fullname = fullname;
+            _count++;
+            Id = _count++;
+        }
+
+        public static bool CheckFullname(string fullname)
+        {
+            fullname = fullname.Trim();
+            if (fullname.Length >= 7 && fullname.Length <= 30 && HasDigit(fullname) == false && SpaceTest(fullname) == true && SecondTest(fullname) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool HasDigit(string fullname)
+        {
+            fullname = fullname.Trim();
+            for (int i = 0; i < fullname.Length; i++)
+            {
+                if (char.IsDigit(fullname[i]) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool SpaceTest(string fullname)
+        {
+            byte count = 0;
+            fullname = fullname.Trim();
+            for (int i = 0; i < fullname.Length; i++)
+            {
+                if (char.IsWhiteSpace(fullname[i]) == true && char.IsUpper(fullname[i+1]) == true)
+                { 
+                    count++;
+                }
+            }
+            if (count == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool SecondTest(string fullname)
+        {
+            byte count = 0;
+            fullname = fullname.Trim();
+            for (int i = 0; i < fullname.Length; i++)
+            {
+                if (char.IsUpper(fullname[i]) == true)
+                {
+                    count++;
+                }
+            }
+            if (count == 2 && char.IsUpper(fullname[0]) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Static - Sealed - Nullable/Program.cs
@@ -0,0 +1,58 @@
+﻿using Static___Sealed___Nullable.Models;
+using System.Runtime.CompilerServices;
+
+namespace Static___Sealed___Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Student class-i yaradin
+             static Count
+             Id
+             Fullname
+             static CheckFullname()
+
+        Hər dəfə Student yarananda Count artmalı və Id constructor işə düşdükdə dinamik olaraq Count-a əsasən təyin olunmalıdır 
+
+        Fullname memberi ad ve soyadi ifade edir, ona gore de fullname deyeri ad ve soyaddan ibaret olmalidir.(Example: Murad Aliyev) ad ve soyadin ilk herfi boyuk ve ortada bosluq olmalidir.
+
+        CheckFullname() - Fullname-nin duzgunluyunu yoxlayan ve geriye boolean type qaytaran static method yazib methodu istifade edirsiz(Yoxlama, deyer constructora oturulmezden evvel program.cs-de edilmelidir).
+
+        Student yaradilarken Fullname gonderilmek mecburiyyetindedir. Ve duzgun deyer daxil edene qeder proqram bitmemelidir (deyer consoledan goturulur ve duz olana qeder dovr etmelidir)
+             */
+            Console.WriteLine("1-Yeni sagird yaradir");
+            Console.WriteLine("0-Emeliyyati sonlandirir");
+            byte input = Convert.ToByte(Console.ReadLine());
+           
+            if (input is 1)
+            {
+                bool response = true;
+                while (response)
+                {
+                    Console.WriteLine("Sagirdin adini ve soyadini daxil edin");
+                    Student student = new Student(Console.ReadLine());
+                    if (Student.CheckFullname(student.Fullname) is true)
+                    {
+                        Console.WriteLine("Sagird ugurla elave edildi");
+                        response = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Daxil etdiyiniz sagirdin adi ve yaxud soyadi dogru daxil edilmemisdir bir daha daxil edin");
+                        response = true;
+                    }
+                }
+            }
+            else if (input is 0)
+            {
+                Console.WriteLine("Emeliyyat basa catdi");
+            }
+            else
+            {
+                Console.WriteLine("Yanlis reqem daxil edildi");
+            }
+        }
+    }
+}
